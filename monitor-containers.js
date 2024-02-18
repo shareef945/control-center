981f79e236bb9fe1942847a64c94c9a45d71bfb4
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-02-18 23:00:29 +0000

    done

--- monitor-containers.js
+++ monitor-containers.js
@@ -1,45 +1,76 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
-var dockerode_1 = require("dockerode");
-var twilio_1 = require("twilio");
+const Dockerode = require("dockerode");
+const Twilio = require("twilio");
+
 // Twilio config
-var accountSid = process.env.TWILIO_SID;
-var authToken = process.env.TWILIO_AUTH_TOKEN;
-var twilioPhoneNumber = process.env.TWILIO_NUMBER;
-var destinationPhoneNumber = process.env.DESTINATION_PHONE_NUMBER;
-var twilioClient = new twilio_1.Twilio(accountSid, authToken);
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_NUMBER;
+const destinationPhoneNumber = process.env.DESTINATION_PHONE_NUMBER;
+const twilioClient = new Twilio(accountSid, authToken);
+
 // Docker setup
-var docker = new dockerode_1.default({ socketPath: "/var/run/docker.sock" });
+const docker = new Dockerode({ socketPath: "/var/run/docker.sock" });
+
+// Function to format date and time
+function formatDateTime(date) {
+  return date.toLocaleString(); // Adjust the formatting as needed
+}
+
 // Function to send SMS
 function sendSms(containerName, status) {
-    twilioClient.messages
-        .create({
-        body: "Container ".concat(containerName, " is ").concat(status, "."),
-        from: twilioPhoneNumber,
-        to: destinationPhoneNumber,
+  const dateTime = formatDateTime(new Date());
+  const statusMessage = status === "start" ? "running" : "stopped";
+  const messageBody = `${containerName} is ${statusMessage} as of ${dateTime}.`;
+
+  twilioClient.messages
+    .create({
+      body: messageBody,
+      from: twilioPhoneNumber,
+      to: destinationPhoneNumber,
     })
-        .then(function (message) { return console.log("Message sent: ".concat(message.sid)); })
-        .catch(function (error) { return console.error(error); });
+    .then((message) => console.log(`Message sent: ${message.sid}`))
+    .catch((error) => console.error(error));
 }
-// Monitor Docker events
-docker.getEvents({
-    filters: {
-        event: ["start", "die"],
-    },
-}, function (err, stream) {
-    if (err) {
-        return console.error(err);
-    }
-    if (!stream) {
-        return console.error("Stream is undefined");
-    }
-    stream.on("data", function (data) {
-        var event = JSON.parse(data.toString());
-        if (event.Type === "container" &&
-            ["start", "die"].includes(event.Action)) {
-            var containerName = event.Actor.Attributes.name;
-            sendSms(containerName, event.Action);
-        }
-    });
+
+// Check Docker connection and send SMS if connected
+docker.ping((err, data) => {
+  if (err) {
+    console.error("Failed to connect to Docker:", err);
+    sendSms("Damen", "stopped");
+  } else {
+    console.log("Successfully connected to Docker:", data);
+  }
 });
+
+// Monitor Docker events
+docker.getEvents(
+  {
+    filters: {
+      event: ["start", "die"],
+    },
+  },
+  (err, stream) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    if (!stream) {
+      return console.error("Stream is undefined");
+    }
+
+    stream.on("data", (data) => {
+      const event = JSON.parse(data.toString());
+
+      if (
+        event.Type === "container" &&
+        ["start", "die"].includes(event.Action)
+      ) {
+        const imageNameWithTag = event.Actor.Attributes.image;
+        const imageName = imageNameWithTag.split(":")[0];
+        const status = event.Action === "start" ? "running" : "stopped";
+        sendSms(imageName, status);
+      }
+    });
+  }
+);
